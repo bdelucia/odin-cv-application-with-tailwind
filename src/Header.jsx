a5886ad2bbe915b13273c2513e88bf4e8f1e75ab
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-05-06 14:22:11 -0700

    header!

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function Header() {
+  return (
+    <div className="flex w-full flex-col">
+      <div className="divider divider-primary">React Resume Maker</div>
+    </div>
+  );
+}
+
+export default Header;
