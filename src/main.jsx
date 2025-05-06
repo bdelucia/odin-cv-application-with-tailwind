a40932d8ba0c5a377d502f4931cc71f76e4d4296
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-05-06 15:03:09 -0700

    personal info!

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+// import App from './App.jsx';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
+import PersonalInfo from './Personalnfo.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex-grow">
-        <App />
+        <PersonalInfo />
       </div>
       <Footer />
     </div>
