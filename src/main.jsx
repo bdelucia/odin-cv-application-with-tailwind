a7eff6cea0f39f9aeec7d0580972ebcb7d572b66
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-05-06 14:48:48 -0700

    footer!

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <App />
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex-grow">
+        <App />
+      </div>
+      <Footer />
+    </div>
   </StrictMode>
 );
