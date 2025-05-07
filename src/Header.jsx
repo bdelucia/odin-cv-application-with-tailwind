aa7eef1b1079954947386a26574b8cddb31b4add
bdelucia <bdelucia@asu.edu>
2025-05-06 23:20:02 -0700

    just in case sudo pacman -Syu fucks up

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,17 @@
-import './index.css';
-
+import "./index.css";
+import ThemeSwitcher from "./ThemeSwitcher";
 function Header() {
   return (
-    <div className="flex w-full flex-col">
-      <div className="divider divider-primary">React Resume Maker</div>
-    </div>
+    <header className="navbar bg-base-200">
+      <div className="navbar-start">
+        <a className="btn btn-ghost normal-case text-xl">My Website</a>
+      </div>
+      <div className="navbar-end p-2">
+        <div id="theme-switcher">
+          <ThemeSwitcher />
+        </div>
+      </div>
+    </header>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 // import App from './App.jsx';
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import PersonalInfo from './Personalnfo.jsx';
-import Education from './Education.jsx';
-import Experience from './Experience.jsx';
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import PersonalInfo from "./Personalnfo.jsx";
+import Education from "./Education.jsx";
+import Experience from "./Experience.jsx";
+import ThemeSwitcher from "./ThemeSwitcher.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="min-h-screen flex flex-col">
       <Header />
