5ee15bfbd479ea191416e273b99cf0e9d35de504
Bobby DeLucia <bdelucia@asu.edu>
2025-05-09 02:11:58 -0700

    data analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/next";
 import "./index.css";
+import { inject } from "@vercel/analytics";
 
+inject();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
