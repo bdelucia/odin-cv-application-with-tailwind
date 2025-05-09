e20eea4985919e45e917e757b2792efabb0b09f9
bdelucia <bdelucia@asu.edu>
2025-05-09 00:25:14 -0700

    resume styling update

--- src/ResumePreview.jsx
+++ src/ResumePreview.jsx
@@ -4,14 +4,16 @@ function ResumePreview({ person, education, experience1, experience2 }) {
   return (
     <>
       <article className="p-2 transition-[border-radius] duration-500 linear prose prose-lg bg-base-200 border-secondary rounded-box w-4xl border">
-        <div className="flex flex-col items-start bg-base-100 p-4">
-          <h1 className="w-full text-center">{person.personalFullName}</h1>
+        <div className="flex flex-col items-start bg-white text-black p-4">
+          <h1 className="w-full text-center text-black">
+            {person.personalFullName}
+          </h1>
           <div className=" w-full flex justify-center gap-10 not-prose">
             <p>{person.personalEmail}</p>
             <p>{person.personalPhone}</p>
           </div>
 
-          <h2>Education</h2>
+          <h2 className="text-black">Education</h2>
           <div className="flex flex-col gap-1">
             <div className="flex justify-between w-full items-center not-prose">
               <div className="prose-lg font-bold">
@@ -24,7 +26,7 @@ function ResumePreview({ person, education, experience1, experience2 }) {
             <div className="prose-lg italic">
               <p>{education.degree}</p>
             </div>
-            <h2>Experience</h2>
+            <h2 className="text-black">Experience</h2>
             <div className="flex justify-between w-full items-center not-prose">
               <div className="prose-lg font-bold">
                 <h3>{experience1.jobTitle}</h3>
